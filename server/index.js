c424267bb85e01b32cc256de79fb4ed7939bbf84
Kris <kriskuiper74@gmail.com>
2020-03-09 14:17:23 +0100

    feat: add styles to homepage

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('module-alias/register')
 
+const path = require('path')
 const express = require('express')
 const { PORT } = require('@lib/constants')
 
@@ -8,6 +9,7 @@ const app = express()
 app.get('/', require('@routes/index'))
 app.get('/:id', require('@routes/detail'))
 
+app.use(express.static(path.join(__dirname, '../client/static')))
 app.use(require('@routes/not-found'))
 app.use(require('@routes/error'))
 
