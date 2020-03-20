08382a5751f0bf48c84bd60b8f40951831b415eb
Kris <kriskuiper74@gmail.com>
2020-03-20 11:36:50 +0100

    feat(service-worker): make use of the fetch

--- src/static/service-worker.js
+++ src/static/service-worker.js
@@ -1,21 +1,19 @@
 const PRECACHE_NAME = 'launcher-precache'
 const RUNTIME_CACHE_NAME = 'launcher-runtime-cache'
-const PRECACHE_URIS = [
-	'/styles/app.css',
-	'/scripts/bundle.js'
-]
+const HTML_CACHE_NAME = 'launcher-html-cache'
+const PRECACHE_ASSETS = ['/styles/app.css', '/offline.html']
 
 // Precache some static assets
 self.addEventListener('install', (event) => {
 	event.waitUntil(
 		caches.open(PRECACHE_NAME)
-			.then(cache => cache.addAll(PRECACHE_URIS))
+			.then(cache => cache.addAll(PRECACHE_ASSETS))
 			.then(() => self.skipWaiting())
 	)
 })
 
 /**
- * Removing outdated caches
+ * Remove outdated caches
  * https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#removing_outdated_caches
  */
 self.addEventListener('activate', (event) => {
@@ -30,16 +28,46 @@ self.addEventListener('activate', (event) => {
 						.map(cacheName => caches.delete(cacheName))
 				)
 			})
+			.then(() => self.clients.claim())
 	)
 })
 
 // Serve assets from cache
-// self.addEventListener('fetch', (event) => {
-// 	// If some asset is cached, serve from cache, else, put the current file
-// 	// in cache and serve just the request url
-// 	const { request } = event
+self.addEventListener('fetch', (event) => {
+	// If some asset is cached, serve from cache, else, put the current file
+	// in cache and serve just the request url
+	const { request } = event
 
-// 	event.respondWith(
+	event.respondWith(
+		fetchAndCache(request, HTML_CACHE_NAME)
+	)
+})
 
-// 	)
-// })
+function fetchAndCache(request, cacheName) {
+	return fetch(request.url)
+		.then(response => {
+			if (!response.ok) {
+				throw new TypeError('Bad response status!')
+			}
+
+			caches.open(cacheName)
+				// You can't fiddle with the original response because
+				// of how things work, so we have to clone the response.
+				.then(
+					cache => cache.put(request, response.clone())
+						.then(() => response)
+				)
+		})
+}
+
+function isCoreGetRequest(request) {
+	return request.method === 'GET' && PRECACHE_ASSETS.includes(getPathName(request))
+}
+
+function getPathName(request) {
+	const url = new URL(request.url)
+
+	return url.pathname
+}
+
+
