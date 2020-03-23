20cf0d44b2ccf4958f602b3ba7e936cbc47760a6
Kris <kriskuiper74@gmail.com>
2020-03-23 13:29:43 +0100

    chore: minify css & js when running a build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,15 @@
+const { terser } = require('rollup-plugin-terser')
+
 module.exports = [
+	{
+		input: 'src/static/scripts/index.js',
+		output: {
+			file: 'dist/scripts/bundle.js'
+		},
+		plugins: [
+			terser()
+		]
+	},
 	{
 		input: 'src/static/service-worker.js',
 		output: {
---
+++ src/static/scripts/index.js
@@ -0,0 +1,5 @@
+console.log(printHelloWorld())
+
+function printHelloWorld() {
+	return 'Hello world'
+}
