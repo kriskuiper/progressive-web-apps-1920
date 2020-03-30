452e0818ca0a3d38d2b4d3a0d4a91fdb08bb9df8
Kris <kriskuiper74@gmail.com>
2020-03-30 09:54:59 +0200

    feat: add comment to client-side js

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,5 +1,8 @@
 console.log(printHelloWorld())
 
+// Sadly my website doesn't need any JS
+// so this is to show that the JS is minified
+// on buildtime
 function printHelloWorld() {
 	return 'Hello world'
 }
